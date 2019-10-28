f4c1e20bb16badfaf8252f289bec4dbf68080207
erick <sistemasoperativos.c1g3@gmail.com>
2019-10-28 03:35:12 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+    constructor(props) {
+        super (props)
 
-export default App;
+        this.state = {
+            personajes: [],
+            isFetch:true,
+        }
+    }
+
+    componentDidMount () {
+        fetch('https://swapi.co/api/people')
+        .then(response => response.json())
+        .then(persJson => this.setState({personajes: persJson.results, isFetch: false}))
+    }
+
+    render() {
+      const personajes = this.state.personajes.map((todo,i) => {
+        return (
+          <div className= "col-md-4">
+              <div className= "card mt-4">
+                <div className= "card-header">
+                  <h2>Nombre: {todo.name}</h2>
+                  <span className= "badge badge-pill badge-danger ml-2"> 
+                  Birth year: {todo.birth_year}
+                  </span>
+                </div>
+                <div className= "card-body">
+                  <p>Genero: {todo.gender}</p>
+                </div>
+              </div>
+          </div>
+        )
+      })
+      
+      return (
+    
+        <div className="App"> 
+          <nav className="navbar navbar-dark bg-dark">
+            <a href="" className="text-white">
+              Personajes de Star Wars
+              <span className="badge badge-pill badge-light ml-2">
+                Cantidad de personajes: {this.state.personajes.length}
+              </span>
+            </a>
+          </nav>
+          
+          <div className="container">
+            <div className="row mt-4">
+              {personajes}
+            </div>
+          </div>
+
+        </div>
+      );
+    }
+  }
+  
+  export default App;
